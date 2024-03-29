0ab7b6ade4b6cc1373c732fa293ae8dfc17181d4
mattlabcode <52426789+mattlabcode@users.noreply.github.com>
2024-03-29 01:48:49 -0400

    Added support for functional testing and displaying global variables

--- nb_utils.py
+++ nb_utils.py
@@ -1,3 +1,26 @@
+from typing import Any
+import doctest
+
 ### Save figs
 
 ### Save drive
+
+### Other
+
+def doctest_function(object: callable, globs: dict[str, Any]) -> None:
+    """Convenience wrapper to run doctests for a specific function or class.
+
+    Parameters
+    ----------
+    object : callable
+        Class, function, or other object with doctests to be run.
+    globs : dict[str, Any]
+        Global variables from module of interest.
+    """
+    print('-------------------------------------------------------')
+    finder = doctest.DocTestFinder(verbose=1, recurse=False)
+    runner = doctest.DocTestRunner(verbose=1)
+    for test in finder.find(obj=object, globs=globs):
+        results = runner.run(test)
+    print('-------------------------------------------------------')
+    print(results)
