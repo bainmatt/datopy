e65ef49095275da2ce97500a0af2a37cf8d8bd5e
bainmatt <52426789+bainmatt@users.noreply.github.com>
2024-04-07 17:53:48 -0400

    Make display function more compact

--- display_dataset.py
+++ display_dataset.py
@@ -17,103 +17,102 @@ from typing import Iterable, Any
 import doctest
 from nb_utils import doctest_function
 
-class display(object):  
-    """
-    Display informative HTML representation of multiple objects side-by-side.
-    
-    Parameters
-    ----------
-    object : str
-        String containing array-like or dataframe variable.
-    *args : list, default=None
-        Additional objects to display alongside the supplied `object`.
 
-    Returns
-    -------
-    result
-        Printout of formal object string representation.
+# TODO archive this (replaced with more concise `eva()`)
+# class display:  
+#     """
+#     Display an informative representation of multiple objects side-by-side.
+    
+#     Parameters
+#     ----------
+#     *args : list
+#         String representation of array-like or dataframe variables to display.
         
-    Examples
-    --------
-    Data frame example:
-    >>> df1 = make_df('AB', [1, 2]); df2 = make_df('AB', [3, 4])
-    >>> display('df1', 'df2', 'pd.concat([df1, df2])').r(globals(), bold=False)
-    <BLANKLINE>
-    df1
-    --- (2, 2) ---
-        A   B
-    1  A1  B1
-    2  A2  B2
-    <BLANKLINE>
-    <BLANKLINE>
-    df2
-    --- (2, 2) ---
-        A   B
-    3  A3  B3
-    4  A4  B4
-    <BLANKLINE>
-    <BLANKLINE>
-    pd.concat([df1, df2])
-    --- (4, 2) ---
-        A   B
-    1  A1  B1
-    2  A2  B2
-    3  A3  B3
-    4  A4  B4
-    <BLANKLINE>
-    <BLANKLINE>
+#     Examples
+#     --------
+#     Data frame example:
+#     >>> df1 = make_df('AB', [1, 2]); df2 = make_df('AB', [3, 4])
+#     >>> display('df1', 'df2', 'pd.concat([df1, df2])').eva(globals(), bold=False)
+#     <BLANKLINE>
+#     df1
+#     --- (2, 2) ---
+#         A   B
+#     1  A1  B1
+#     2  A2  B2
+#     <BLANKLINE>
+#     <BLANKLINE>
+#     df2
+#     --- (2, 2) ---
+#         A   B
+#     3  A3  B3
+#     4  A4  B4
+#     <BLANKLINE>
+#     <BLANKLINE>
+#     pd.concat([df1, df2])
+#     --- (4, 2) ---
+#         A   B
+#     1  A1  B1
+#     2  A2  B2
+#     3  A3  B3
+#     4  A4  B4
+#     <BLANKLINE>
+#     <BLANKLINE>
     
-    Matrix example:
-    >>> A = np.array([[1, 3], [2, 4]]); x = np.array([[0, 1]])
-    >>> display("A", "x.T", "np.dot(A, x.T)").r(globals(), bold=False)
-    <BLANKLINE>
-    A
-    --- (2, 2) ---
-    array([[1, 3],
-           [2, 4]])
-    <BLANKLINE>
-    <BLANKLINE>
-    x.T
-    --- (2, 1) ---
-    array([[0],
-           [1]])
-    <BLANKLINE>
-    <BLANKLINE>
-    np.dot(A, x.T)
-    --- (2, 1) ---
-    array([[3],
-           [4]])
-    <BLANKLINE>
-    <BLANKLINE>
     
-    """
-    
-    def __init__(self, *args, globs: dict[str, Any] = None, bold: bool = True):
-        self.args = args
-    
-    # TODO ? move `globs` arg to constructor as class instance and 
-    # ? use __repr__ to execute upon construction
-    def r(self, globs: dict[str, Any] = None, bold: bool = True):    
-        """
-        Shorthand for `__repr__()`.
+#     # allowed_variables = ['np', 'my_var1', 'my_var2']
 
-        Parameters
-        ----------
-        globs : dict[str, Any], default=None
-            Global namespace, for access to eval() for nonlocals passed by name.
-        bold : bool, default=True
-            Option to disable string styling for testing purposes.
-        """
-        output = ""
-        for arg in self.args:
-            name = '\033[1m' + arg + '\033[0m' if bold else arg
-            value = eval(arg, globs)
-            shape = np.shape(value)
-            rounded_value = np.round(value, 2)
-            output += f"\n{name}\n--- {repr(shape)} ---\n{repr(rounded_value)}\n\n"
+#     # Restrict the globals() dictionary to only include allowed variables
+#     # restricted_globals = {key: value for key, value in globals().items() if key in ['A', 'x', 'np', 'pd']}
 
-        print(output)
-        return None
+    
+#     Matrix example:
+#     >>> A = np.array([[1, 3], [2, 4]]); x = np.array([[0, 1]])
+#     >>> display("A", "x.T", "np.dot(A, x.T)").eva(globals(), bold=False)
+#     <BLANKLINE>
+#     A
+#     --- (2, 2) ---
+#     array([[1, 3],
+#            [2, 4]])
+#     <BLANKLINE>
+#     <BLANKLINE>
+#     x.T
+#     --- (2, 1) ---
+#     array([[0],
+#            [1]])
+#     <BLANKLINE>
+#     <BLANKLINE>
+#     np.dot(A, x.T)
+#     --- (2, 1) ---
+#     array([[3],
+#            [4]])
+#     <BLANKLINE>
+#     <BLANKLINE>
+    
+#     """
+    
+#     def __init__(self, *args):
+#         self.args = args
+    
+#     # TODO ? move `globs` arg to constructor as class instance
+    
+#     def eva(self, globs: dict[str, Any] = None, bold: bool = True):    
+#         """
+#         Parameters
+#         ----------
+#         globs : dict[str, Any], default=None
+#             Global namespace, for access to eval() for nonlocals passed by name.
+#         bold : bool, default=True
+#             Option to disable string styling for testing purposes.
+#         """
+#         output = ""
+#         for arg in self.args:
+#             name = '\033[1m' + arg + '\033[0m' if bold else arg
+#             value = np.round(eval(arg, globs), 2)
+#             shape = np.shape(value)
+#             output += f"\n{name}\n--- {repr(shape)} ---\n{repr(value)}\n\n"
+
+#         print(output)
+#         return None
 
 
 def make_df(cols: Iterable[Any], ind: Iterable[Any]) -> pd.DataFrame:
@@ -151,10 +150,90 @@ def make_df(cols: Iterable[Any], ind: Iterable[Any]) -> pd.DataFrame:
     return pd.DataFrame(data, ind)    
 
 
+def eva(*args, globs: dict[str, Any] = None, bold: bool = True):    
+    """
+    Display an informative representation of multiple objects side-by-side.
+    
+    Parameters
+    ----------
+    args : tuple
+        Tuple of expressions to evaluate and display.
+    globs : dict[str, Any], default=None
+        Global namespace, to give eval() access to nonlocals passed by name.
+    bold : bool, default=True
+        Option to enable/disable string styling.
+        
+    Examples
+    --------
+    Data frame example:
+    >>> df1 = make_df('AB', [1, 2]); df2 = make_df('AB', [3, 4])
+    >>> eva('df1', 'df2', 'pd.concat([df1, df2])', globs=globals(), bold=False)
+    <BLANKLINE>
+    df1
+    --- (2, 2) ---
+        A   B
+    1  A1  B1
+    2  A2  B2
+    <BLANKLINE>
+    <BLANKLINE>
+    df2
+    --- (2, 2) ---
+        A   B
+    3  A3  B3
+    4  A4  B4
+    <BLANKLINE>
+    <BLANKLINE>
+    pd.concat([df1, df2])
+    --- (4, 2) ---
+        A   B
+    1  A1  B1
+    2  A2  B2
+    3  A3  B3
+    4  A4  B4
+    <BLANKLINE>
+    <BLANKLINE>
+    
+    Matrix example:
+    >>> A = np.array([[1, 3], [2, 4]]); x = np.array([[0, 1]])
+    >>> eva("A", "x.T", "np.dot(A, x.T)", globs=globals(), bold=False)
+    <BLANKLINE>
+    A
+    --- (2, 2) ---
+    array([[1, 3],
+           [2, 4]])
+    <BLANKLINE>
+    <BLANKLINE>
+    x.T
+    --- (2, 1) ---
+    array([[0],
+           [1]])
+    <BLANKLINE>
+    <BLANKLINE>
+    np.dot(A, x.T)
+    --- (2, 1) ---
+    array([[3],
+           [4]])
+    <BLANKLINE>
+    <BLANKLINE>
+    """
+    if globs is None:
+        globs = {}    
+        
+    output = ""
+    for arg in args:
+        name = '\033[1m' + arg + '\033[0m' if bold else arg
+        value = np.round(eval(arg, globs), 2)
+        shape = np.shape(value)
+        output += f"\n{name}\n--- {repr(shape)} ---\n{repr(value)}\n\n"
+
+    print(output)
+    return None
+
+
 def main():
     # Comment out (2) to run all tests in script; (1) to run specific tests
     # doctest.testmod(verbose=True)
-    doctest_function(display, globs=globals())
+    doctest_function(eva, globs=globals())
     return None
 
 if __name__ == "__main__":
