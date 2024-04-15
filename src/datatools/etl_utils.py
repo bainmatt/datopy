618595fbb72542217f3087f8ac16a7a7c1229120
bainmatt <52426789+bainmatt@users.noreply.github.com>
2024-04-14 20:38:43 -0400

    Linting and clearer custom typing

--- src/datatools/etl_utils.py
+++ src/datatools/etl_utils.py
@@ -1,5 +1,5 @@
-""" 
-Tools for efficient web-based data retrieval, raw data processing, constructing tables, and populating missing metadata fields in existing tables. 
+"""
+Tools for efficient web-based data retrieval, raw data processing, constructing tables, and populating missing metadata fields in existing tables.
 """
 
 import re
@@ -8,15 +8,18 @@ import doctest
 import wptools
 import pandas as pd
 from pydantic import BaseModel, Field
-from typing import Any, Annotated, Callable, List, NamedTuple
+from typing import Any, Annotated, Callable, Generic, List, NamedTuple, TypeVar
 
 from workflow_utils import doctest_function
+
+# Custom types
+# TODO archive (define type variables (type(arg_in) == type(arg_out)))
+# T = TypeVar("T")
 
 
 # ---------------
 # --- Extract ---
 # ---------------
-
 
 
 # -----------------
@@ -30,14 +33,14 @@ def omit_string_patterns(input_string: str, patterns: List[str]) -> str:
     Parameters
     ----------
     input_string : str
-        The to-be-cleaned string. 
-    patterns : List[str] 
+        The to-be-cleaned string.
+    patterns : List[str]
         A list of patterns to omit from the string.
 
     Returns
     -------
     str : The input string with the supplied patterns ommitted.
-        
+
     Examples
     --------
     >>> input_string = "[[A \\\\ messy * string * with undesirable /patterns]]"
@@ -57,7 +60,6 @@ def omit_string_patterns(input_string: str, patterns: List[str]) -> str:
 # ------------
 
 
-
 # -----------------------
 # --- Topic retrieval ---
 # -----------------------
@@ -67,7 +69,7 @@ def omit_string_patterns(input_string: str, patterns: List[str]) -> str:
 def retrieve_wiki_topics(listing_page: str, verbose: bool = True) -> List[str]:
     """
     _summary_
-    
+
     Find listing_page here: https://en.wikipedia.org/wiki/List_of_lists_of_lists
 
     Parameters
@@ -75,34 +77,33 @@ def retrieve_wiki_topics(listing_page: str, verbose: bool = True) -> List[str]:
     listing_page : str
         _description_
     verbose : bool, default=True
-        _description_ 
+        _description_
 
     Returns
     -------
     target_pages : List[str]
         _description_
     """
-    
+
     wiki_parse = wptools.page(listing_page).get_parse().data['parsetree']
 
     regex_pattern = re.compile("\[\[(.*?)\]\]")
     matches = regex_pattern.findall(wiki_parse)
     pages = [match.split('|')[0].strip() for match in matches]
     target_pages = pages[4:-3]
-    
+
     if verbose:
         pprint.pp(target_pages)
-    
+
     return target_pages
 
 # listing_page = "List of legendary creatures from China"
 # retrieve_wiki_topics(listing_page)
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     # Comment out (2) to run all tests in script; (1) to run specific tests
     doctest.testmod(verbose=True)
     # doctest_function(get_film_metadata, globs=globals())
-        
-    ## One-off tests
-    
+
+    # One-off tests
